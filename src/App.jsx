3ee87793d4a1d6116258b7c27a3773cf3b68fa2f
NathanD0 <122122847+nathanbrz@users.noreply.github.com>
2024-08-16 18:33:55 -0600

    Working with two way binding inputs

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
-
+import Player from "./components/Player";
 function App() {
-  
 
   return (
-    <h1>React Tic-Tac-Toe</h1>
-  )
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <Player name="Player 1:" symbol="X" />
+          <Player name="Player 2:" symbol="O" />
+        </ol>
+        <p>gameboard</p>
+      </div>
+      <p>Log</p>
+    </main>
+  );
 }
 
-export default App
+export default App;
